c8077fe7804db2302f24d46f747c98efefeb894d
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-01-09 16:13:29 +0000

    datatypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+
+// number = 2 
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol = unique
+
+
+// object
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
+
+
+
+
+
