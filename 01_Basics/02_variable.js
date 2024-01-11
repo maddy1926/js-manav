884bb75a2b0c60f5f08f844b3ce83d8610f9e044
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-01-11 16:07:34 +0000

    Conversion

---
+++ 01_Basics/02_variable.js
@@ -0,0 +1,21 @@
+const accountId = 639736701
+let accountEmail = "manav@google.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+let accountState;
+// accountId = 2
+
+
+accountEmail = "jdkj@gmail.com"
+accountPassword = "dwffwdf"
+accountCity = "Noida"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use Var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+// "33" =>Number
+//""33abc" => Nan
+//true =>1 ; false =>0
+
+ 
+
+let isloggedin = "Manav"
+
+let booleanisloggedin = Boolean(isloggedin);
+//console.log(booleanisloggedin);
+
+let stringNumber1 = 33
+
+let StringNumber = String(stringNumber1)
+
+console.log(typeof StringNumber);
