802745919eb120ec7e62cb110abc3cfaecf403b3
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-01-14 18:43:20 +0000

    nums and maths

--- 01_Basics/06_numsandmaths.js
+++ 01_Basics/06_numsandmaths.js
@@ -1,19 +1,43 @@
 const score = 400
 
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toFixed(4));
+//console.log(balance.toFixed(4));
 
 const otherNumber = 1123.8966
 
-console.log(otherNumber.toPrecision(3));
+//console.log(otherNumber.toPrecision(3));
 
 const oneNumber = 1000000000
 
-console.log(oneNumber.toLocaleString('en-IN'));
+//console.log(oneNumber.toLocaleString('en-IN'));
+
+
+// ************* MATHS **************
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-70));
+// console.log(Math.round(2.5));
+// console.log(Math.ceil(2.5));
+// console.log(Math.floor(2.5));
+// console.log(Math.min(4, 3, 5, 6, 9));
+// console.log(Math.max(4, 7, 8, 2, 6));
+
+// console.log(Math.random());    //0 and 1 ke andar va;ue deti hai
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 5
+const max = 15
+
+console.log(Math.floor((Math.random() * (max - min ) + min)));
+
+
+
 
 
 
