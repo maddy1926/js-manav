c4290b48ebf7617ddfcc1c8ce8ed25ff7b927c2f
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-01-14 19:14:28 +0000

    datesinjs

---
+++ 01_Basics/07_datesinjs.js
@@ -0,0 +1,29 @@
+//Dates
+
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let newDate = new Date(2024, 9, 19)
+// console.log(newDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+
+
+// console.log(newDate.getTime());
+
+
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+newDate.toLocaleString('default', {
+weekday: "long",
+
+}')
+
+
