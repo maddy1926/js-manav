ffd7eb4238d37c3ad8115150c5d28a4dcf731c46
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-01-14 14:46:06 +0000

    numsandmaths

---
+++ 01_Basics/06_numsandmaths.js
@@ -0,0 +1,21 @@
+const score = 400
+
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toFixed(4));
+
+const otherNumber = 1123.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const oneNumber = 1000000000
+
+console.log(oneNumber.toLocaleString('en-IN'));
+
+
+
+
+
