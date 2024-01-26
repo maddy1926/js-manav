a1b97623bed8131725036ca0873827e40cabc500
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-01-26 19:11:10 +0000

    object4

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,26 @@
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.age = 28
+
+// console.log(tinderuser);
+
+
+const regularuser = {
+
+    fullname: {
+        userfullname:{
+            firstname: "Manav",
+            lastname: "Dixit", 
+        }
+    }
+}
+
+// console.log(typeof regularuser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = {...obj1, ...obj2}
+
+console.log(obj3);
