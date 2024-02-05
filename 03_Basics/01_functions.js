c43e8a3faf64ca0537ba821998908652509d3304
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-02-05 07:33:41 +0000

    function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -17,4 +17,36 @@ function loginUserMessage(Username = "Manav"){
     return `${Username} just logged in`
 }
 
-console.log(loginUserMessage("Rubal"));
+// console.log(loginUserMessage("Rubal"));
+
+
+function CalculatecartPrice(...num1){
+           return num1
+}
+
+
+// console.log(CalculatecartPrice(4000, 5000, 6000))
+
+//How to call an Object in Functions. 
+
+const User = {
+    username: "Manav Dixit",
+    Age: 29
+}
+
+function Details(anyDetails){
+            console.log(`Username is ${anyDetails.username} and age is ${anyDetails.Age}`);
+}
+
+Details(User)
+
+//How to call an array in function
+
+
+const myArray = [10, 20, 30, 40, 50]
+
+function secondArray(getArray){
+   return getArray[4]
+}
+
+console.log(secondArray(myArray));
