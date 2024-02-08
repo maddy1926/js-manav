598e1e91f558bcac1499416d3cf40f80d11f1dc8
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2024-02-08 04:54:45 +0000

    revision

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -1,13 +1,13 @@
 let score = false
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueinnumber = Number(score)
-//console.log(typeof valueinnumber);
+// console.log(typeof valueinnumber);
 //console.log(valueinnumber);
 
-// "33" =>Number
+//  "33" => Number
 //""33abc" => Nan
 //true =>1 ; false =>0
 
@@ -16,13 +16,13 @@ let valueinnumber = Number(score)
 let isloggedin = "Manav"
 
 let booleanisloggedin = Boolean(isloggedin);
-//console.log(booleanisloggedin);
+// console.log(booleanisloggedin);
 
 let stringNumber1 = 33
 
 let StringNumber = String(stringNumber1)
 
-//console.log(typeof StringNumber);
+// console.log(typeof StringNumber);
 
 
 
